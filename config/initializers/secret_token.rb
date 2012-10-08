e9dc9046503947536dadbd45d4f0d0d53ed68dbb
Alexander Rudenko <mur.mailbox@gmail.com>
2012-10-08 08:08:34 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AjaxTutor::Application.config.secret_token = '871b1259778374a601d34249179007a87ef1bc32668a66e6a1c4ee686eb91253f602ce0f299b0048d87a9849db56082e5ff9ab1ea1de88a653c035397fd96483'
